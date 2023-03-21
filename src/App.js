953bd23be44c7c1943092a425dbb5b148eaf8a73
Dario Quinteros <dario.quinteros.bustos@gmail.com>
2023-03-20 22:57:11 -0300

    Eliminando comments

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-//A diferencia de los elementos del DOM de los navegadores, los elementos de React son objetos planos, y su creación es de bajo costo. React DOM se encarga de actualizar el DOM para igualar los elementos de React.
-
 import './App.css';
 import React, {Component, Fragment} from "react";
 //Components
 import NavBar from "./components/NavBar";
 import Card from "./components/Card";
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-
-/*export default function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Nav />
-      <Nav />
-    </div>
-  );
-}*/
-
-// Extends sirve para extender funcionalidad por intermedio de la herencia 
-
 class App extends Component {
-  //Conceptualmente, los componentes son como las funciones de JavaScript. Aceptan entradas arbitrarias (llamadas “props”) y retornan elementos de React que describen lo que debe aparecer en la pantalla.
-  render () { //Permite retornar a JSX
-    return ( // Si se desea devolver más de un elemento JSX se tienen que envolver en un <div>
-      /*<div> 
-        <h1>Hello World!</h1>
-        <h2>Hello World!</h2>
-      </div>*/
-      /*<Fragment className="App">
-        <NavBar />
-      </Fragment>*/
-
-      /*Otra forma sería:
-      <>
-        <h1>Hello World!</h1>
-        <h2>Hello World!</h2>
-      </>
-      */ 
+  render () { 
+    return ( 
       <>
         <NavBar />
         <div className= "CardClass">
@@ -62,13 +19,4 @@ class App extends Component {
   }
 }
 
-export default App; //Export default sirve para cuando se desea exportar un componente. SOLAMENTE SE PUEDE HACER UN EXPORT DEFAULT POR FICHERO
-
-/* También se pueden exportar variables 
-export let age = 25
-export const name = "Daniel"
----> Para importar:
-import {name} from "./App"
----> O:
-import {name, age, my-variable, function} from "./App"
-*/
+export default App; 
